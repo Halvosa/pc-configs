269b4e18707d43cef331e5aaa7d3432f8a40f696
Halvosa <61124050+Halvosa@users.noreply.github.com>
2022-11-27 01:32:51 +0100

    Update configure.sh

--- configure.sh
+++ configure.sh
@@ -3,3 +3,10 @@
 cp vimrc $HOME/.vimrc
 cp tmux.conf $HOME/.tmux.conf
 cp vscode/settings.json $HOME/.config/Code/User/settings.json
+
+
+# Firewall
+sudo ufw allow 1714:1764/udp  # kde/gs connect
+sudo ufw allow 1714:1764/tcp
+
+sudo ufw reload
